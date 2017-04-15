0ac0ca8027e22fc835b41d531fd5ccddecb915fc
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-15 19:43:00 +0400

    add description :four:

--- AGAv1.5WPF/AGAv1.5WPF/MainWindow.xaml.cs
+++ AGAv1.5WPF/AGAv1.5WPF/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AGAv1._5WPF
 {
@@ -97,7 +85,7 @@ namespace AGAv1._5WPF
             }
             Calculator = SumPoint / SumCredit;
             MessageBox.Show(Calculator.ToString());
-            L: MessageBox.Show("Կրկին կարող եք փորձել");
+            L: MessageBox.Show("Կրկին կարող եք փորձել !!!");
         }
     }
 }

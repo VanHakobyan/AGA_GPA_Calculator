f026e6051d5fda48adf3e29fb6d8e54100359057
VanHakobyan <van19962013@mail.ru>
2017-05-13 16:31:44 +0400

    add new window :+1:

--- AGAv1.5WPF/AGAv1.5WPF/MainWindow.xaml.cs
+++ AGAv1.5WPF/AGAv1.5WPF/MainWindow.xaml.cs
@@ -18,7 +18,36 @@ namespace AGAv1._5WPF
         {
             InitializeComponent();
         }
-     
+        public void DataSaving(float Calculator)
+        {
+            SqlConnectionStringBuilder conStr = new SqlConnectionStringBuilder();
+            conStr.DataSource = @"(localdb)\MSSQLLocalDB";
+            conStr.InitialCatalog = "AGA";
+            conStr.IntegratedSecurity = true;
+            SqlConnection connetion = new SqlConnection(conStr.ConnectionString);
+            string cmdText = "INSERT INTO Students(Name,AGA,Date) VALUES (@UserName,@calculator,@date)";
+            SqlCommand cmd = new SqlCommand(cmdText, connetion);
+            //cmd.Parameters.AddWithValue("ID", 1);
+            cmd.Parameters.AddWithValue("UserName", UserName.Text);
+            cmd.Parameters.AddWithValue("calculator", Calculator);
+            cmd.Parameters.AddWithValue("date", DateTime.Now);
+            cmd.CommandType = CommandType.Text;
+
+            try
+            {
+                connetion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connetion != null)
+                    connetion.Close();
+            }
+        }
 
         private async void Calculator_Click(object sender, RoutedEventArgs e)
         {
@@ -101,34 +130,8 @@ namespace AGAv1._5WPF
                 }
             }
             Calculator = SumPoint / SumCredit;
-
-            SqlConnectionStringBuilder conStr = new SqlConnectionStringBuilder();
-            conStr.DataSource = @"(localdb)\MSSQLLocalDB";
-            conStr.InitialCatalog = "AGA";
-            conStr.IntegratedSecurity = true;
-            SqlConnection connetion = new SqlConnection(conStr.ConnectionString);
-            string cmdText = "INSERT INTO Students(Name,AGA,Date) VALUES (@UserName,@calculator,@date)";
-            SqlCommand cmd = new SqlCommand(cmdText,connetion);
-            //cmd.Parameters.AddWithValue("ID", 1);
-            cmd.Parameters.AddWithValue("UserName", UserName.Text);
-            cmd.Parameters.AddWithValue("calculator", Calculator);
-            cmd.Parameters.AddWithValue("date", DateTime.Now);
-            cmd.CommandType = CommandType.Text;
-
-            try
-            {
-                connetion.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                if (connetion != null)
-                    connetion.Close();
-            }
+            DataSaving(Calculator);
+           
             await Task.Run(() => MessageBox.Show("Ձեր ՄՈԳ-ը կազմում է`" + Calculator.ToString()));
             L: await Task.Run(() => MessageBox.Show("Կարող եք կրկին հաշվել!!!"));
         }
@@ -150,9 +153,10 @@ namespace AGAv1._5WPF
             await Task.Run(() => MessageBox.Show("Ծրագրի հեղինակն է Վան Հակոբյանը"));
         }
 
-        private void UserName_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            UserName.Text = null;
-        }
+        private void UserName_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)  => UserName.Text = null;
+
+
+        private void Show_Click(object sender, RoutedEventArgs e) => new Results().Show();
+
     }
 }
---
+++ AGAv1.5WPF/AGAv1.5WPF/Results.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AGAv1._5WPF
+{
+    /// <summary>
+    /// Interaction logic for Results.xaml
+    /// </summary>
+    public partial class Results : Window
+    {
+        public Results()
+        {
+            InitializeComponent();
+            //SqlConnection 
+            //SqlDataReader read = new SqlDataReader();
+        }
+    }
+}
